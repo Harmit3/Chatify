d1c307074ddd9fc0d9db3a73b7c664810538eb0e
Harmit Patel <harmitpatel110@gmail.com>
2025-01-21 02:16:10 -0500

    1.Left-Panel UI Hardcoded completed. 2.Right-Panel UI Hardcoded completed. 3.UI for this web application is completed.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+
 
 const inter=Inter({subsets:["latin"]});
 
