50cfd040052217e24353d3d8aa0410fae958af15
Harmit Patel <harmitpatel110@gmail.com>
2025-01-21 23:59:48 -0500

    Added auth. as a clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig={
+    providers: [
+      {
+        domain: "https://happy-sturgeon-3.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ],
+};
+
+export default authConfig;
