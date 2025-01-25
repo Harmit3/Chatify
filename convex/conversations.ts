346f4bbef23247023747a72c5e1000143b6b06c4
Harmit Patel <harmitpatel110@gmail.com>
2025-01-25 02:39:30 -0500

    "1.getUsers filtered w. All error implimented with right code"

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,5 +1,5 @@
 import {ConvexError,v} from  "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const createConversation=mutation({
     args:{
@@ -49,7 +49,13 @@ export const createConversation=mutation({
     },
 });
 
-
+export const getrMyConversations=query({
+    args:{},
+    handler:async(ctx,args)=>{
+                    
+        const identity=await ctx.auth.getUserIdentity();
+    },
+});
 
 export const generateUploadUrl=mutation(async(ctx)=>{
     return await ctx.storage.generateUploadUrl();
--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -1,4 +1,4 @@
-"user client";
+"use client";
 import { ListFilter,Search} from "lucide-react";
 import { Input } from "../ui/input";
 import ThemeSwitch from "./theme-switch";
