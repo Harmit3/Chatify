e7fc69851c546b2104f7af65a8e3ddf89d1f3130
Harmit Patel <harmitpatel110@gmail.com>
2025-01-28 20:26:28 -0500

    1.Images and Videos mutation added                                              2.sendImages and sendVideos stored in db                                        3.Completed media-dropdown component (react-player used for videos)

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+/**@type {import('next').NextConfig}*/
+
+import { hostname } from 'os';
+
+const nextConfig= {
+  images:{
+    remotePatterns:[
+      {hostname:"confident-monitor-488.convex.cloud"}
+    ]
+  }
+};
+
+export default nextConfig;
